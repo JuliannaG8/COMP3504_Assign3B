0ec27b91970cb2aebb0007bddfc8c9306688cf51
Julianna <gagliardijulianna@gmail.com>
2021-11-30 15:09:20 -0700

    imported and rendered Item component

--- assign3b/src/components/ItemList.js
+++ assign3b/src/components/ItemList.js
@@ -1,16 +1,23 @@
 import {createRef} from "react";
+import Item from "./Item";
 
 const ItemList = (props)=>{
     const search = createRef();
     const handleSearch = ()=>{
         if (typeof search === "undefined")
             alert("No search parameters given")
+        else
+            props.search(search);
     }
     return(
         <div>
             <label>Search items by name or id</label>
             <input type="text" name="search"/>
-            <button onClick={handleSearch}/>
+            <button onClick={handleSearch}>Search</button>
+            <button onClick={props.reset}>Reset</button>
+            <ul>
+                {props.items.map(item=><Item item={item} key={item.id}/>)}
+            </ul>
         </div>
     )
 }
