9ee583b726490de59f2462caad3a26ad371f2007
Julianna <gagliardijulianna@gmail.com>
2021-11-30 12:38:21 -0700

    added search and reset functions

--- assign3b/src/App.js
+++ assign3b/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import {useEffect, useState} from "react";
 import {Route} from "react-router-dom";
+import {cloneDeep} from "lodash";
 
 function App() {
-  const [items, updateItems] = useState([])
+  const [items, updateItems] = useState([]);
+  const [itemsCopy, updateItemsCopy] = useState([]);
   useEffect(()=>{
     if (items.length === 0){
       const url = "https://comp3504api.herokuapp.com/api/items";
@@ -14,13 +16,24 @@ function App() {
           throw new Error("Fetch Failed");
       }).then(data=>{
         updateItems(data);
+        updateItemsCopy(data);
       })
     }
   })
+    const reset = ()=>{updateItemsCopy(items)}
+    const search = (params)=>{
+      const copy = cloneDeep(items);
+      const matches = copy.filter((i)=> i.name.toLowerCase().includes(params) || i.id === params);
+      if (matches.length === 0)
+        alert("No items with name or id " + params);
+      else
+          updateItemsCopy(matches);
+
+    }
   return (
     <div className="App">
       <Route path="/" exact>
-        <ItemList items={items}/>
+        <ItemList items={itemsCopy} reset={reset} search={search}/>
       </Route>
       <Route path="/addItem" exact>
         <AddItemForm/>
