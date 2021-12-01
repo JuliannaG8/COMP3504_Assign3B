364d23dd7f70031b9c4fb295d67e9678affb7afe
Julianna Gagliardi <35346454+JuliannaG8@users.noreply.github.com>
2021-11-30 17:46:32 -0700

    Delete assign3b directory

--- assign3b/src/components/Item.js
+++
@@ -1,8 +0,0 @@
-import {Link} from "react-router-dom";
-
-const Item = props=>{
-    return(
-        <li><Link to={`/item/${props.item.id}`}>{props.item.id} {props.item.name}</Link></li>
-    )
-}
-export default Item;
